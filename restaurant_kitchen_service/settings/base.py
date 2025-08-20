12720acb6b7cc1cc8ec01a36be0f84c8c5d4ee33
msi <lg.mobile0808@gmail.com>
2025-08-20 14:44:43 +0300

    add postgres db

--- restaurant_kitchen_service/settings/base.py
+++ restaurant_kitchen_service/settings/base.py
@@ -17,7 +17,7 @@ import os
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -27,11 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 if not SECRET_KEY:
     raise ValueError("SECRET_KEY must be set in environment variables!")
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-
-ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
@@ -78,16 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'restaurant_kitchen_service.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from kitchen import views
-from restaurant_kitchen_service import settings
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
