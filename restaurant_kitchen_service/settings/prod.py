b5e35a989208ad81a249443ffbdb0c1764a034f7
msi <lg.mobile0808@gmail.com>
2025-08-23 15:27:30 +0300

    add requirements.txt and README.md

--- restaurant_kitchen_service/settings/prod.py
+++ restaurant_kitchen_service/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 import os
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "restaurant-kitchen-service-njiu.onrender.com",
